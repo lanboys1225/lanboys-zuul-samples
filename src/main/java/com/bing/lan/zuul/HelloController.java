0d0f660791299454133e5504fe5d7cd92e327579
lanboys1225 <lanboys1225@gmial.com>
2020-05-09 17:39:26 +0800

    添加自定义拦截器

--- src/main/java/com/bing/lan/zuul/HelloController.java
+++ src/main/java/com/bing/lan/zuul/HelloController.java
@@ -1,5 +1,8 @@
 package com.bing.lan.zuul;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/a/foo")
     public String a() {
+        logger.info("/a/foo");
         return "a";
     }
 
     @RequestMapping("/b/foo")
     public String b() {
+        logger.info("/b/foo");
         return "b";
+    }
+
+    @RequestMapping("/c/foo")
+    public String c(@RequestHeader("uid") Long userId) {
+        logger.info("/c/foo: {}", userId);
+        return "c";
     }
 }
